4371a0143d7823c1c29df33534d3ca7aa6c0f763
GeebeeFrost <omgbolahan1@gmail.com>
2024-04-03 12:05:55 +0100

    Basic Webpack setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,13 @@
+import $ from "jquery";
+
+const p1 = document.createElement("p");
+const p2 = document.createElement("p");
+const p3 = document.createElement("p");
+
+p1.textContent = "Holberton Dashboard";
+p2.textContent = "Dashboard data for the students";
+p3.textContent = "Copyright - Holberton School";
+
+document.body.appendChild(p1);
+document.body.appendChild(p2);
+document.body.appendChild(p3);
