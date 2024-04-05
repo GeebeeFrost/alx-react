50713e38b6f5376c960fa2541fdfc638d25ffc18
GeebeeFrost <omgbolahan1@gmail.com>
2024-04-05 14:54:30 +0100

    Create basic React application

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import logo from "./holberton-logo.jpg";
 
 function App() {
   return (
     <>
       <div className="App-header">
+        <div className="logo">
+          <img src={logo} alt="logo" className="App-logo" />
+        </div>
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
