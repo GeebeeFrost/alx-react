486e6a7829b51fc3d2136a5d2b24b2369d20cdff
GeebeeFrost <omgbolahan1@gmail.com>
2024-04-08 11:04:44 +0100

    Deploy to a GitHub page

---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import logo from "./holberton-logo.jpg";
+import { getFooterCopy, getFullYear } from "./utils";
+
+const footer = `Copyright ${getFullYear()} - ${getFooterCopy(true)}`;
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <div className="logo">
+          <img src={logo} alt="logo" className="App-logo" />
+        </div>
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" placeholder="Email"></input>
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"></input>
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>{footer}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
