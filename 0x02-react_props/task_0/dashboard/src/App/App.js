bf58d379cb232bb49bb6c0533ef6bf325ec45f70
GeebeeFrost <omgbolahan1@gmail.com>
2024-04-08 14:48:03 +0100

    Break App into components

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -1,33 +1,18 @@
 import React from "react";
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 import "./App.css";
-import logo from "../assets/holberton-logo.jpg";
-import { getFooterCopy, getFullYear } from "../utils/utils";
-
-const footer = `Copyright ${getFullYear()} - ${getFooterCopy(true)}`;
 
 function App() {
   return (
     <>
-      <div className="App-header">
-        <div className="logo">
-          <img src={logo} alt="logo" className="App-logo" />
-        </div>
-        <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email: </label>
-        <input type="email" id="email" name="email" placeholder="Email"></input>
-        <label htmlFor="password">Password: </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"></input>
-        <button>OK</button>
-      </div>
-      <div className="App-footer">
-        <p>{footer}</p>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
       </div>
     </>
   );
--- 0x02-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.test.js
@@ -7,19 +7,4 @@ describe("App Component Tests", () => {
     const app = shallow(<App />);
     expect(app.exists()).toBe(true);
   });
-
-  it("renders a div with class App-header", () => {
-    const app = shallow(<App />);
-    expect(app.find(".App-header").exists()).toBe(true);
-  });
-
-  it("renders a div with class App-body", () => {
-    const app = shallow(<App />);
-    expect(app.find(".App-body").exists()).toBe(true);
-  });
-
-  it("renders a div with class App-footer", () => {
-    const app = shallow(<App />);
-    expect(app.find(".App-footer").exists()).toBe(true);
-  });
 });
