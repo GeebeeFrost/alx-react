a8783ae9e9c48a5da6bb05a4490609359cc5ec19
GeebeeFrost <omgbolahan1@gmail.com>
2024-04-26 13:25:32 +0100

    Context consumer & advanced state

---
+++ 0x05-react_state/task_3/dashboard/src/BodySection/BodySection.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { shallow } from "enzyme";
+import BodySection from "./BodySection";
+import { StyleSheetTestUtils } from "aphrodite";
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Normal rendering tests", () => {
+  it("should render one h2 element and children correctly", () => {
+    const bodySectionWrapper = shallow(
+      <BodySection title="test title">
+        <p>test children node</p>
+      </BodySection>
+    );
+    expect(bodySectionWrapper.exists()).toBe(true);
+    expect(bodySectionWrapper.find("h2").text()).toBe("test title");
+    expect(bodySectionWrapper.find("p").text()).toBe("test children node");
+  });
+});
