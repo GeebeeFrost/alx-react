87493bad2512cba042fe034d6f00694addd4dc93
GeebeeFrost <omgbolahan1@gmail.com>
2024-05-08 16:43:01 +0100

    Create a reducer for Courses

---
+++ 0x08-react_redux_reducer_selector/task_2/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,25 @@
+import * as actions from "./courseActionTypes";
+
+export function selectCourse(index) {
+  return {
+    type: actions.SELECT_COURSE,
+    index,
+  };
+}
+
+export function unSelectCourse(index) {
+  return {
+    type: actions.UNSELECT_COURSE,
+    index,
+  };
+}
+
+export function fetchCourseSuccess(data) {
+  return {
+    type: actions.FETCH_COURSE_SUCCESS,
+    data,
+  };
+}
+
+export const boundSelectCourse = (index) => dispatch(selectCourse(index));
+export const boundUnSelectCourse = (index) => dispatch(unSelectCourse(index));
